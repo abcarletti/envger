ffb9b25f933ce16408e0b270b926af4a2e0df9b6
Angel Bolado Carletti <bolado.angel@gmail.com>
2025-02-09 22:49:11 +0100

    fix: grid groups

--- src/components/project-groups.tsx
+++ src/components/project-groups.tsx
@@ -105,7 +105,7 @@ export default function ProjectGroups() {
 	return (
 		<>
 			{!isLoadingGroups && groupsComposed.length > 0 && (
-				<div className="grid grid-cols-1 xl:grid-cols-2 gap-2">
+				<div className="grid grid-cols-1 xl:grid-cols-2 gap-2 w-full">
 					{groupsComposed}
 				</div>
 			)}
